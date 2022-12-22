3111a26d01d6552e3055ce2a0a373f84448e5e41
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-22 18:32:36 +0100

    comments added

--- candies.py
+++ candies.py
@@ -16,35 +16,49 @@ import sys
 #  2. INTEGER_ARRAY arr
 #
 
+# TODO: valid assignment checker function
 def auxiliary_isSafe(arr, c):
+    # make a check through all candies in the array
     for i in range(len(arr) - 1):
+        # rating (i) < rating (i+1) ==> candies (i) < candies (i+1)
         if arr[i] < arr[i + 1]:
             if not c[i] < c[i + 1]:
                 return False
+        # rating (i) > rating (i+1) ==> candies (i) < candies (i+1)
         if arr[i] > arr[i + 1]:
             if not c[i] > c[i + 1]:
                 return False
+        # rating (i) == rating (i+1) ==> candies (i) > candies (i+1)
         if arr[i] == arr[i + 1]:
             if not c[i] > c[i + 1]:
                 return False
     return True
 
 
+# TODO: recursive backtracking function
 def auxiliary_candies(arr, c, possibleCandies, cur, pos):
     # base case
-    if pos == len(arr)-1:
+    if pos == len(arr) - 1:
         if auxiliary_isSafe(arr, c):
+            # valid assignment, return no. of candies
             return sum(c)
         else:
+            # invalid assignment, return infinity
             return 10000000000
 
+    # BFS candy positioning
     for element in possibleCandies:
+        # put candy to the candies array
         c[pos + 1] = element
+        # DFS via recursion to place new candies, step case
         cur = min(cur, auxiliary_candies(arr, c, possibleCandies, cur, pos + 1))
+        # backtrack
         c[pos + 1] = 0
+    # return solution to candies function
     return cur
 
 
+# TODO: main function
 def candies(arr, c, possibleCandies):
     cur = 10000000000
     result = auxiliary_candies(arr, c, possibleCandies, cur, -1)
