c4b20042e65d15d0ea6a992a9b9319125da9b788
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-23 17:40:25 +0100

    correct and functional  backtracking algorithm completed, isValid() function added for solution validation

--- coin change exercise/coin.py
+++ coin change exercise/coin.py
@@ -5,6 +5,7 @@ import os
 import random
 import re
 import sys
+from collections import Counter
 
 
 #
@@ -16,25 +17,51 @@ import sys
 #  2. LONG_INTEGER_ARRAY c
 #
 
-# TODO: solution validation function
-def isValid():
-    return 0
+# def isValid(changeArr, goodSoFar):
+
+
+# TODO: solution validation algorithm
+def isValid(changeArr, goodsSoFar):
+    for arr in goodsSoFar:
+        counter1 = Counter(changeArr)
+        counter2 = Counter(arr)
+        if counter1 == counter2:
+            return False
+    return True
 
 
 # TODO: recursive backtracking algorithm
-def coinBacktrack():
-    return 0
+def coinBacktrack(n, c, changeArr, goodsSoFar, count):
+    if sum(changeArr) >= n:
+        if sum(changeArr) == n and isValid(changeArr, goodsSoFar):
+            count += 1
+            goodsSoFar.append([x for x in changeArr])
+            return count
+        else:
+            return count
+
+    for coin in c:
+        changeArr.append(coin)
+        count = coinBacktrack(n, c, changeArr, goodsSoFar, count)
+        changeArr.pop(len(changeArr) - 1)
+
+    return count
 
 
 def getWays(n, c):
     # Write your code here
-    return 0
+    return coinBacktrack(n, c, [], [], 0)
 
 
 if __name__ == '__main__':
-    n = 4
-    c = [1, 2, 3]
+    n = 10
+    c = [2, 5, 3, 6]
 
     # Print the number of ways of making change for 'n' units using coins having the values given by 'c'
-
     print(getWays(n, c))
+
+# print(f'base case: count += 1 done, good goodsSoFar : {goodsSoFar}')
+# print(f'base case: count += 0 done, bad goodsSoFar: {goodsSoFar}')
+# print(f'changeArr appended: {changeArr}')
+# print(f'count: {count}')
+# print(f'changeArr popped: {changeArr}')
