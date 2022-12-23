446609fc0d63261b0f89a312d8f539574c535fab
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-23 20:34:34 +0100

    template and structure readied

---
+++ goodland electricity/goodland.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'pylons' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+# TODO: solution validation algorithm
+def isValid():
+    return 0
+
+
+# TODO: recursive backtracking algorithm
+def goodlandBacktrack():
+    return 0
+
+
+def pylons(k, arr):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    n = 0
+    k = 0
+    arr = []
+
+    print(pylons(k, arr))
