99933e07d774731c52f35678429d57405b915eca
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-23 21:39:15 +0100

    isValid function readied, backtracking function mostly done

--- goodland electricity/goodland.py
+++ goodland electricity/goodland.py
@@ -6,7 +6,6 @@ import random
 import re
 import sys
 
-
 #
 # Complete the 'pylons' function below.
 #
@@ -16,24 +15,53 @@ import sys
 #  2. INTEGER_ARRAY arr
 #
 
+cur = 10000000000000000
+
+
 # TODO: solution validation algorithm
-def isValid():
-    return 0
+def isValid(k, plantConfig, plant_indices):
+    isCovered = [0 for i in range(len(plantConfig))]
+    for index in plant_indices:
+        for i_left in range(k - 1):
+            if index - (i_left + 1) < 0:
+                break
+            isCovered[index - (i_left + 1)] = 1
+        for i_right in plant_indices:
+            if index - (i_right + 1) >= len(isCovered):
+                break
+            isCovered[index + (i_right + 1)] = 1
+    if sum(isCovered) == len(isCovered):
+        return True
+    else:
+        return False
 
 
 # TODO: recursive backtracking algorithm
-def goodlandBacktrack():
-    return 0
+def goodlandBacktrack(k, plantConfig, pos, plant_indices):
+    if pos == plant_indices[len(plant_indices) - 1]:
+        if isValid(k, plantConfig, plant_indices):
+            return sum([1 if x == 1 else 0 for x in plantConfig])
+
+    for x in [0, 1]:
+        plantConfig[plant_indices[pos + 1]] = 1
+        cur = min(cur, goodlandBacktrack(k, plantConfig, plant_indices[pos + 1], plant_indices))
+        plantConfig[plant_indices[pos + 1]] = 0
+    return cur
 
 
 def pylons(k, arr):
-    # Write your code here
-    return 0
+    plant_indices = []
+    for i in range(len(arr)):
+        if arr[i] == 1:
+            plant_indices.append(i)
+    return goodlandBacktrack(k, [-2 if x == 0 else -1 for x in arr], plant_indices[0] - 1, plant_indices)
 
 
 if __name__ == '__main__':
     n = 0
     k = 0
-    arr = []
-
-    print(pylons(k, arr))
+    arr = [0, 1, 1, 1, 0, 0, 0]
+    # print(pylons(k, arr))
+    m = 3
+    for i in range(m - 1):
+        print(i)
