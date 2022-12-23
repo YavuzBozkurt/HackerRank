cea141c43324e4307be1435b886020b1a29bf479
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-23 17:50:53 +0100

    comments added for clarifications

--- coin change exercise/coin.py
+++ coin change exercise/coin.py
@@ -23,33 +23,43 @@ from collections import Counter
 # TODO: solution validation algorithm
 def isValid(changeArr, goodsSoFar):
     for arr in goodsSoFar:
+        # count how many of each element there are in arrays changeArr and arr \in goodsSoFar
         counter1 = Counter(changeArr)
         counter2 = Counter(arr)
         if counter1 == counter2:
+            # identical change with different permutation only, return false
             return False
+    # new and unique change
     return True
 
 
 # TODO: recursive backtracking algorithm
 def coinBacktrack(n, c, changeArr, goodsSoFar, count):
+    # base case
     if sum(changeArr) >= n:
+        # is solution valid
         if sum(changeArr) == n and isValid(changeArr, goodsSoFar):
+            # valid solution
             count += 1
+            # add it to goodsSoFar so that an identical change with different permutation is not re-counted
             goodsSoFar.append([x for x in changeArr])
             return count
         else:
+            # invalid solution
             return count
 
     for coin in c:
         changeArr.append(coin)
         count = coinBacktrack(n, c, changeArr, goodsSoFar, count)
+        # backtrack
         changeArr.pop(len(changeArr) - 1)
-
+    # return count to previous call
     return count
 
 
 def getWays(n, c):
     # Write your code here
+    # initialize count to be 0
     return coinBacktrack(n, c, [], [], 0)
 
 
@@ -60,8 +70,3 @@ if __name__ == '__main__':
     # Print the number of ways of making change for 'n' units using coins having the values given by 'c'
     print(getWays(n, c))
 
-# print(f'base case: count += 1 done, good goodsSoFar : {goodsSoFar}')
-# print(f'base case: count += 0 done, bad goodsSoFar: {goodsSoFar}')
-# print(f'changeArr appended: {changeArr}')
-# print(f'count: {count}')
-# print(f'changeArr popped: {changeArr}')
