361c4a9f456ac03ba9d22a2f770cbb23fe9a99ce
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-23 23:49:52 +0100

    correct and functional backtracking complete

--- goodland electricity/goodland.py
+++ goodland electricity/goodland.py
@@ -6,6 +6,7 @@ import random
 import re
 import sys
 
+
 #
 # Complete the 'pylons' function below.
 #
@@ -15,21 +16,22 @@ import sys
 #  2. INTEGER_ARRAY arr
 #
 
-cur = 10000000000000000
-
-
 # TODO: solution validation algorithm
 def isValid(k, plantConfig, plant_indices):
     isCovered = [0 for i in range(len(plantConfig))]
     for index in plant_indices:
-        for i_left in range(k - 1):
-            if index - (i_left + 1) < 0:
-                break
-            isCovered[index - (i_left + 1)] = 1
-        for i_right in range(k-1):
-            if index + (i_right + 1) >= len(isCovered):
-                break
-            isCovered[index + (i_right + 1)] = 1
+        if plantConfig[index] == 1:
+            isCovered[index] = 1
+            for i_left in range(k - 1):
+                if index - (i_left + 1) < 0:
+                    break
+                isCovered[index - (i_left + 1)] = 1
+            for i_right in range(k - 1):
+                if index + (i_right + 1) >= len(isCovered):
+                    break
+                isCovered[index + (i_right + 1)] = 1
+        else:
+            continue
     if sum(isCovered) == len(isCovered):
         return True
     else:
@@ -37,14 +39,16 @@ def isValid(k, plantConfig, plant_indices):
 
 
 # TODO: recursive backtracking algorithm
-def goodlandBacktrack(k, plantConfig, pos, plant_indices):
-    if pos == plant_indices[len(plant_indices) - 1]:
+def goodlandBacktrack(k, plantConfig, pos, plant_indices, cur):
+    if pos == len(plant_indices) - 1:
         if isValid(k, plantConfig, plant_indices):
             return sum([1 if x == 1 else 0 for x in plantConfig])
+        else:
+            return cur
 
     for x in [0, 1]:
-        plantConfig[plant_indices[pos + 1]] = 1
-        cur = min(cur, goodlandBacktrack(k, plantConfig, plant_indices[pos + 1], plant_indices))
+        plantConfig[plant_indices[pos + 1]] = x
+        cur = min(cur, goodlandBacktrack(k, plantConfig, pos + 1, plant_indices, cur))
         plantConfig[plant_indices[pos + 1]] = 0
     return cur
 
@@ -54,11 +58,16 @@ def pylons(k, arr):
     for i in range(len(arr)):
         if arr[i] == 1:
             plant_indices.append(i)
-    return goodlandBacktrack(k, [-2 if x == 0 else -1 for x in arr], plant_indices[0] - 1, plant_indices)
+    return goodlandBacktrack(k, [-2 if x == 0 else -1 for x in arr], -1, plant_indices, 1000000000000)
 
 
 if __name__ == '__main__':
-    n = 0
-    k = 0
-    arr = [0, 1, 1, 1, 0, 0, 0]
-    # print(pylons(k, arr))
+    k = 3
+    arr = [0, 1, 0, 0, 0, 0, 1]
+    print(pylons(k, arr))
+
+
+# print(f'BASE CASE HIT: plantConfig: {plantConfig}, cur: {cur}')
+# print(f' sum would be : {sum([1 if x == 1 else 0 for x in plantConfig])} where set = {[1 if x == 1 else 0 for x in plantConfig]}')
+# print(f'plantConfig: {plantConfig}, cur: {cur}')
+# print(f'BASE CASE HIT: plantConfig: {plantConfig}, cur: {cur}')
