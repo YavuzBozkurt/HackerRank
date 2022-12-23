5a01e281cb7765b97d4da4a5aad53835cb69d102
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-23 13:10:35 +0100

    template and structure readied

---
+++ coin change exercise/coin.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'getWays' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. LONG_INTEGER_ARRAY c
+#
+
+# TODO: solution validation function
+def isValid():
+    return 0
+
+
+# TODO: recursive backtracking algorithm
+def coinBacktrack():
+    return 0
+
+
+def getWays(n, c):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    n = 4
+    c = [1, 2, 3]
+
+    # Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+    print(getWays(n, c))
