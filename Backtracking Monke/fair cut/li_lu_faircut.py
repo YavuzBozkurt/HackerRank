8d9cf42037be9f7312e001c9414071a95bf38505
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-24 11:39:04 +0100

    comments added for clarifications

--- Backtracking Monke/fair cut/li_lu_faircut.py
+++ Backtracking Monke/fair cut/li_lu_faircut.py
@@ -18,23 +18,31 @@ import sys
 
 # TODO: auxiliary fairness computation algorithm
 def auxiliary_unfair(unfairArr, k):
+    # create a set of sets identify li's and lu's integers
     li_and_lu = [[val for val in unfairArr[0:k]], [val for val in unfairArr[k:len(unfairArr)]]]
+    # initialize unfairness to be 0
     unfairness = 0
     for x in li_and_lu[0]:
         for y in li_and_lu[1]:
+            # compute unfairness for given x, y pair
             unfairness += abs(x - y)
+    # return unfairness magnitude
     return unfairness
 
 
 # TODO: recursive backtracking algorithm
 def fairBacktrack(k, arr, unfairArr, pos, cur):
+    # base case
     if pos == len(unfairArr) - 1:
+        # compute unfairness magnitude
         return auxiliary_unfair(unfairArr, k)
 
     for element in arr:
+        # select candidate
         unfairArr[pos + 1] = element
-        index_record = arr.index(element)
+        # compute the minimum unfairness magnitude so far
         cur = min(cur, fairBacktrack(k, [x for x in arr[arr.index(element) + 1:]], unfairArr, pos + 1, cur))
+        # backtrack
         unfairArr[pos + 1] = 0
     return cur
 
