a6d4d37459e7a677ad939123f582cc2b58b26d20
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-25 19:30:30 +0100

    template added. methodology added

---
+++ Backtracking Monke/prime xor/prime.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'primeXor' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+# TODO: solution validation algorithm
+def xorIsPrime():
+    return 0
+
+
+# TODO: recursive backtracking algorithm
+def XorBacktrack():
+    return 0
+
+
+def primeXor(a):
+    return 0
+
+
+if __name__ == '__main__':
+    q = 1
+    n = 3
+    a = [3511, 3671, 4153]
+    print(primeXor(q, n, a))
