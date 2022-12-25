c61c74c07dd59037171bda8165efd339e26983a3
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-25 21:26:18 +0100

    correct and functional backtracking algorithm complete

--- Backtracking Monke/prime xor/prime.py
+++ Backtracking Monke/prime xor/prime.py
@@ -5,6 +5,7 @@ import os
 import random
 import re
 import sys
+from collections import Counter
 
 
 #
@@ -14,18 +15,70 @@ import sys
 # The function accepts INTEGER_ARRAY a as parameter.
 #
 
+# TODO: auxiliary algorithm to check if a given value is prime
+def isPrime(val):
+    if val < 2:
+        return False
+
+    for number in range(2, val):
+        # if val is divisible by number, then val is not prime
+        if val % number == 0:
+            return False
+
+    # all checks passed, thus, val is prime
+    return True
+
+
 # TODO: solution validation algorithm
-def xorIsPrime():
-    return 0
+def xorIsPrime(solutions, multiset, a):
+
+    for element in solutions:
+        count1 = Counter(element)
+        count2 = Counter(multiset)
+        if count1 == count2:
+            return False
+
+    considered = [val for val in a]
+    for i in range(len(multiset)):
+        if multiset[i] in considered:
+            considered[considered.index(multiset[i])] = -1000000000
+        else:
+            return False
+
+    xor_result = 0
+    for value in multiset:
+        xor_result = xor_result ^ value
+
+    if isPrime(xor_result):
+        return True
+    else:
+        return False
 
 
 # TODO: recursive backtracking algorithm
-def XorBacktrack():
-    return 0
+def XorBacktrack(n, a, multiset, solutions, cur):
+
+    if len(multiset) >= 1:
+        if xorIsPrime(solutions, multiset, a):
+            solutions.append([x for x in multiset])
+            cur += 1
+        else:
+            cur += 0
+
+    # base case
+    if len(a) == len(multiset):
+        return cur
+
+    for i in range(len(a)):
+        multiset.append(a[i])
+        cur = XorBacktrack(n, a, multiset, solutions, cur)
+        del multiset[-1]
+
+    return cur % (10**9 + 7)
 
 
-def primeXor(a):
-    return 0
+def primeXor(q, n, a):
+    return XorBacktrack(n, a, [], [], 0)
 
 
 if __name__ == '__main__':
