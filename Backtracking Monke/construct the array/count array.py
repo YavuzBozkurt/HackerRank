d74d2212100981ad97e9d7f3f07cddf0723d2181
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-25 18:00:20 +0100

    methodology added, template added

---
+++ Backtracking Monke/construct the array/count array.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'countArray' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER x
+#
+
+# TODO: solution validation algorithm
+def isValid():
+    return 0
+
+
+# TODO: recursive backtracking algorithm
+def arrBacktracking():
+    return 0
+
+
+def countArray(n, k, x):
+    return 0
+    # Return the number of ways to fill in the array.
+
+
+if __name__ == '__main__':
+    n = 0
+    k = 0
+    x = 0
+
+    print(countArray(n, k, x))
