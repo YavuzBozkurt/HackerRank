0fcf72a9869a582b6fe34790a6af819ddc0cd34a
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-25 18:23:14 +0100

    comments added for clarifications

--- Backtracking Monke/construct the array/count array.py
+++ Backtracking Monke/construct the array/count array.py
@@ -20,34 +20,44 @@ import sys
 # TODO: solution validation algorithm
 def isValid(arr, x):
 
+    # if the first element is not 0, then the configuration is invalid
     if arr[0] != 1:
         return False
 
+    # if the last element is not x, then the configuration is invalid
     if arr[len(arr)-1] != x:
         return False
 
+    # if there exists any pair of non-unique consecutive elements, then the configuration is invalid
     for i in range(len(arr)-1):
         if arr[i] == arr[i+1]:
             return False
 
+    # all checks passed, configuration is valid
     return True
 
 
 # TODO: recursive backtracking algorithm
 def arrBacktracking(arr, k, x, pos, cur):
-
+    # base case
     if pos == n-1:
         if isValid(arr, x):
+            # found a valid configuration, update number of solutions by one
             cur += 1
             return cur
         else:
+            # invalid configuration, do not update the number of solutions
             return cur
 
+    # select a candidate
     for val in [i+1 for i in range(k)]:
+        # put the candidate in the array
         arr[pos+1] = val
         cur = arrBacktracking(arr, k, x, pos + 1, cur)
+        # backtrack by overwriting the candidate with 0
         arr[pos+1] = 0
 
+    # return the number of solutions
     return cur
 
 
