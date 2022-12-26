769c6b0c26b7fd9a9e37f46820b780d42749046d
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-26 23:25:27 +0100

    Update

---
+++ Dynamic Monke/sherlock merge/sherlock.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'arrayMerging' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY m as parameter.
+#
+
+# TODO: solution validation algorithm
+def isValid():
+    return 0
+
+
+# TODO: dynamic programming algorithm
+def sherlockDP():
+    return 0
+
+
+def arrayMerging(m):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    m = [1, 2, 3]
+    print(arrayMerging(m))
