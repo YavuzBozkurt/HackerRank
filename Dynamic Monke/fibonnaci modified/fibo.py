1303d22c25c4edc5d02888bfc5d279b7bf5bc75a
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-26 15:01:06 +0100

    template and methodology added

---
+++ Dynamic Monke/fibonnaci modified/fibo.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'fibonacciModified' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER t1
+#  2. INTEGER t2
+#  3. INTEGER n
+#
+
+# TODO: dynamic programming algorithm
+def fiboDP():
+    return 0
+
+
+def fibonacciModified(t1, t2, n):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1 = 0
+    t2 = 0
+    n = 0
+    print(fibonacciModified(t1, t2, n))
