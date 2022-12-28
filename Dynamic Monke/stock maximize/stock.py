1c06a14cd2379ec7b0bd5822818afd4b2bcfb773
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-28 13:42:30 +0100

    methodology and file template added

---
+++ Dynamic Monke/stock maximize/stock.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'stockmax' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY prices as parameter.
+#
+
+# TODO: dynamic programming algorithm
+def stockDP():
+    return 0
+
+
+def stockmax(prices):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    print(stockmax([1, 3, 1, 2]))
