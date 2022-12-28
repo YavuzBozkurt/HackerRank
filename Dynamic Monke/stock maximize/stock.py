dff78058d7ca1a9d249f77491c8da3f8d3a7cc70
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-12-28 14:48:54 +0100

    functional dynamic programming algorithm

--- Dynamic Monke/stock maximize/stock.py
+++ Dynamic Monke/stock maximize/stock.py
@@ -15,14 +15,35 @@ import sys
 #
 
 # TODO: dynamic programming algorithm
-def stockDP():
-    return 0
+def stockDP(prices, goods, T):
+    # base case
+    T[0] = 0
+    for i in range(1, len(T)):
+        # if there exists a future pinnacle point, buy good
+        if len([prices[j] for j in range(i, len(prices)) if prices[j] > prices[i-1]]) >= 1:
+            T[i] = T[i-1] - prices[i-1]
+            # to indicate that you bought it, add "+" sign
+            goods.append("+")
+        # == 0
+        else:
+            # either at a pinnacle point in stock
+            if len(goods) >= 1:
+                T[i] = T[i-1] + prices[i-1]*len(goods)
+                # sold all goods
+                goods = []
+            # or at a point in which trading in future is not favourable for now (i.e., buying causes loss for now)
+            # for profit optimization, we don't but in these cases
+            else:
+                # don't do trading for this day
+                T[i] = T[i-1]
+                continue
+    return T[len(T)-1]
 
 
 def stockmax(prices):
     # Write your code here
-    return 0
+    return stockDP(prices, [], [-1 for i in range(len(prices) + 1)])
 
 
 if __name__ == '__main__':
-    print(stockmax([1, 3, 1, 2]))
+    print(stockmax([5, 1, 5, 10, 2, 1, 2]))
